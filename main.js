3d336c56e31573c11d0f07ef42f31d294ed19e93
Alexandros Chatzis <Alexandros_chatzis@yahoo.com>
2024-03-03 22:16:59 +0200

    Contitional logic now disables fields

--- main.js
+++ main.js
@@ -19,39 +19,36 @@ jQuery(document).ready(function ($) {
   });
 
   if ($("input[type='radio'][name='presence']:checked").val() === "yes") {
-    $(".rsvp-question").closest(".form-group").show();
-    $("input[name='companion_presence']").closest(".form-group").show();
-    $("input[name='companion_name']").closest(".form-group").show();
+    $(".rsvp-question").attr("disabled", false);
+    $("input[name='companion_presence']").attr("disabled", false);
+    $("input[name='companion_name']").attr("disabled", false);
   } else {
-    $(".rsvp-question").hide();
-    $("input[name='companion_presence']").closest(".form-group").hide();
-    $("input[name='companion_name']").closest(".form-group").hide();
+    $("input[name='companion_presence']").attr("disabled", true);
+    $("input[name='companion_name']").attr("disabled", true);
   }
 
   if (
     $("input[type='radio'][name='companion_presence']:checked").val() === "yes"
   ) {
     $("input[name='companion_name']").attr("required", true);
-    $("input[name='companion_name']").closest(".form-group").show();
+    $("input[name='companion_name']").attr("disabled", false);
   } else {
     $("input[name='companion_name']").attr("required", false);
-    $("input[name='companion_name']").closest(".form-group").hide();
+    $("input[name='companion_name']").attr("disabled", true);
   }
 
   $("input[type='radio'][name='presence']").on("change", function () {
     if ($(this).val() === "yes") {
-      $(".rsvp-question").fadeIn();
       const companionName = $("input[name='companion_name']");
       const companionPresence = $("input[name='companion_presence']:checked");
-      companionPresence.closest(".form-group").fadeIn();
+      $("input[name='companion_presence']").attr("disabled", false);
       if (companionPresence.val() === "yes")
-        companionName.closest(".form-group").fadeIn();
+        companionName.attr("disabled", false);
     } else {
-      $(".rsvp-question").fadeOut();
-      $("input[name='companion_presence']").closest(".form-group").fadeOut();
+      $("input[name='companion_presence']").attr("disabled", true);
       $("input[name='companion_name']")
-        .closest(".form-group")
-        .fadeOut()
+        .attr("disabled", true)
+        .parent()
         .find(".jsCustomPlaceholder")
         .removeClass("on-top");
       $("input[name='companion_name']").attr("required", false).val("");
@@ -60,12 +57,12 @@ jQuery(document).ready(function ($) {
 
   $("input[type='radio'][name='companion_presence']").on("change", function () {
     if ($(this).val() === "yes") {
-      $("input[name='companion_name']").closest(".form-group").fadeIn();
+      $("input[name='companion_name']").attr("disabled", false);
       $("input[name='companion_name']").attr("required", true);
     } else {
       $("input[name='companion_name']")
-        .closest(".form-group")
-        .fadeOut()
+        .attr("disabled", true)
+        .parent()
         .find(".jsCustomPlaceholder")
         .removeClass("on-top");
       $("input[name='companion_name']").attr("required", false).val("");
@@ -91,11 +88,11 @@ jQuery(document).ready(function ($) {
     $.each(formDataArray, function (index, field) {
       formData[field.name] = field.value;
     });
-    console.log(formData);
-    const { guest_name } = formData;
-    const { presence } = formData;
-    const { companion_presence } = formData;
-    const { companion_name } = formData;
+
+    const { guest_name = "" } = formData;
+    const { presence = "no" } = formData;
+    const { companion_presence = "no" } = formData;
+    const { companion_name = "" } = formData;
 
     $.ajax({
       url: `https://docs.google.com/forms/d/e/1FAIpQLSfMlbXeLoOHHnpex-nYEF8v824OnXi_NLhlhwNnCxFffZcgPA/formResponse?&submit=Submit?usp=pp_url&entry.1597741806=${guest_name}&entry.877086558=${presence}&entry.1588402917=${companion_presence}&entry.2141206115=${companion_name}`,
@@ -106,7 +103,6 @@ jQuery(document).ready(function ($) {
       },
       error: function (xhr, status, error) {
         // Handle errors
-        console.error(xhr, status, error);
       },
     });
   });
